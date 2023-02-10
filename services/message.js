d2c8e078cbc50734ced86f9bf9d6eab9a7d51378
unknown <your_email_address@example.com>
2023-02-10 11:52:48 +0530

    Message Bug Fix

--- services/message.js
+++ services/message.js
@@ -1,6 +1,6 @@
 const cron = require("node-cron")
 const twilio = require("twilio")
-const message = () => cron.schedule("0 8 * * *", () => { //10seconds
+const message = () => cron.schedule("0 8 * * *", () => { //8AM everyday
 	sendMessage()
 })
 
